6579559352481abf70ab36131d0d0a60656781c7
DonMB77 <purposefuldrifter@gmail.com>
2025-02-20 10:57:28 +0100

    Create O_n.java

---
+++ src/datastructures/big_o/O_n.java
@@ -0,0 +1,11 @@
+package datastructures.big_o;
+
+// example of O(n)
+public class O_n {
+
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+}
