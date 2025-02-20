7958ec6aa18a47c5541d2ea58066e46819587af3
DonMB77 <purposefuldrifter@gmail.com>
2025-02-20 13:12:08 +0100

    Update O_NSquared.java

--- src/datastructures/big_o/O_NSquared.java
+++ src/datastructures/big_o/O_NSquared.java
@@ -1,4 +1,12 @@
 package datastructures.big_o;
 
 public class O_NSquared {
+
+    public static void printItems(int n) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+    }
 }
