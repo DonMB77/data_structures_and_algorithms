58d915f5267926e162047e4d4ef9a8ebf1aac560
DonMB77 <purposefuldrifter@gmail.com>
2025-02-20 13:03:09 +0100

    O(n^2) & O(n) challenge

---
+++ src/datastructures/big_o/O_NSquared.java
@@ -0,0 +1,4 @@
+package datastructures.big_o;
+
+public class O_NSquared {
+}
---
+++ src/datastructures/big_o/challenges/O_n_challenge.java
@@ -0,0 +1,15 @@
+package datastructures.big_o.challenges;
+
+public class O_n_challenge {
+
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+
+    // DO NOT CHANGE THE MAIN METHOD BELOW
+    public static void main(String[] args) {
+        printItems(10);
+    }
+}
