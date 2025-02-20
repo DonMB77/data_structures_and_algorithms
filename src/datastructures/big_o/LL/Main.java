82e98cbfb372b985468d752a8d60e461dfd070c9
DonMB77 <purposefuldrifter@gmail.com>
2025-02-20 18:22:44 +0100

    LinkedList.printList()

--- src/datastructures/big_o/LL/Main.java
+++ src/datastructures/big_o/LL/Main.java
@@ -3,6 +3,7 @@ package datastructures.big_o.LL;
 public class Main {
 
     public static void main(String[] args) {
-        //
+        LinkedList myLL = new LinkedList(4);
+        myLL.printList();
     }
 }
