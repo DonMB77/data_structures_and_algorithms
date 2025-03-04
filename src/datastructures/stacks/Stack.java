75d54894e1f9c60e036a09cd92adee0052a53800
DonMB77 <purposefuldrifter@gmail.com>
2025-03-04 19:59:05 +0100

    Stack pop()

--- src/datastructures/stacks/Stack.java
+++ src/datastructures/stacks/Stack.java
@@ -19,6 +19,17 @@ public class Stack {
         }
     }
 
+    public Node pop() {
+        if (height == 0) {
+            return null;
+        }
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+        return temp;
+    }
+
     public void push(int value) {
         Node newNode = new Node(value);
         if (height == 0) {
