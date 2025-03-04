d75f1a461963d51342c94b630747b6400f8c3810
DonMB77 <purposefuldrifter@gmail.com>
2025-03-04 20:37:58 +0100

    Queue constr + main

---
+++ src/datastructures/queues/Main.java
@@ -0,0 +1,8 @@
+package datastructures.queues;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/datastructures/queues/Queue.java
@@ -0,0 +1,42 @@
+package datastructures.queues;
+
+public class Queue {
+    Node first;
+    Node last;
+    int length;
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    class Node {
+        Node next;
+        int value;
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getLast() {
+        System.out.println("Last: " + last.value);
+    }
+
+    public void getFirst() {
+        System.out.println("First: " + first.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+}
