419d13951774aa612d2c27fdd0c478c4e8499757
DonMB77 <purposefuldrifter@gmail.com>
2025-03-06 19:48:11 +0100

    BinarySearchTree insert() + contains()

--- src/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -2,7 +2,7 @@ package datastructures.binarysearchtree;
 
 public class BinarySearchTree {
 
-    private Node root;
+    Node root;
 
     class Node {
         int value;
@@ -11,6 +11,62 @@ public class BinarySearchTree {
 
         private Node(int value) {
             this.value = value;
+        }
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (temp.value == value) {
+                return false;
+            }
+            if (temp.value < value) {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                } else {
+                    temp = temp.right;
+                }
+            }
+            if (temp.value > value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                } else {
+                    temp = temp.left;
+                }
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (root == null) {
+            return false;
+        }
+        Node temp = root;
+        while (true) {
+            if (temp.value == value) {
+                return true;
+            }
+            if (temp.value > value) {
+                if (temp.left == null) {
+                    return false;
+                } else {
+                    temp = temp.left;
+                }
+            }
+            if (temp.value < value) {
+                if (temp.right == null) {
+                    return false;
+                } else {
+                    temp = temp.right;
+                }
+            }
         }
     }
 }
